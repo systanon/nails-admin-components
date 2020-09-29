8ac31063b0717924423b95ef3894de52cdb433cc
Sergei <tustanovskiis@gmail.com>
2020-09-29 18:27:39 +0300

    library card

--- src/index.js
+++ src/index.js
@@ -1,2 +1,35 @@
-export { default as CourseCard } from './components/CourseCard.vue'
-export { default as CourseCardDetail } from './components/CourseCardDetail.vue'
+import CourseCard  from './components/CourseCard.vue'
+import CourseCardDetail  from './components/CourseCardDetail.vue'
+
+// export default { CourseCard, CourseCardDetail }
+
+
+import Vue from 'vue'
+
+const Components = {
+  CourseCard,
+  CourseCardDetail
+}
+
+Object.keys(Components).forEach(name => {
+  Vue.component(name, Components[name])
+})
+
+export default Components
+
+// const ComponentLibrary = {
+//   install(Vue, options = {}) {
+//     // components
+//     for (const componentName in components) {
+//       const component = components[componentName]
+
+//       Vue.component(component.name, component)
+//     }
+//   }
+// }
+
+// export default ComponentLibrary
+
+// if (typeof window !== 'undefined' && window.Vue) {
+//   window.Vue.use(ComponentLibrary)
+// }
