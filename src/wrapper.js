18427b44f04d6afba3ea83df6c987c6e32ac2394
Sergei <tustanovskiis@gmail.com>
2020-09-29 13:01:25 +0300

    create module without cli

---
+++ src/wrapper.js
@@ -0,0 +1,22 @@
+import CourseCard from './components/CourseCard.vue'
+
+export function install(Vue) {
+  if (install.installed) return;
+  install.installed = true;
+  Vue.component('CourseCard', CourseCard);
+}
+
+const plugin = {
+  install
+};
+let GlobalVue = null;
+if (typeof window !== 'undefined') {
+  GlobalVue = window.Vue;
+} else if (typeof global !== 'undefined') {
+  GlobalVue = global.Vue;
+}
+if (GlobalVue) {
+  GlobalVue.use(plugin);
+}
+
+export default CourseCard;
